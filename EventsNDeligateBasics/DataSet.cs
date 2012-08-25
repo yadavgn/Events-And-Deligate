3da253c9d9fa15541b9c2bdcdb9170d86fc187f6
yadavgn <yadav.ghanshyam@gmail.com>
2012-08-25 19:37:44 +0530

    Created a console application to under stand basics of Deligate and Events

---
+++ EventsNDeligateBasics/DataSet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventsNDeligateBasics
+{
+    public class DataSet 
+    {
+        // Delegate to handle Data Change events.
+        public delegate long DataChangedHandler();
+
+        private int _A;
+        public int A
+        {
+            get { return _A; }
+            set
+            {
+                _A = value;
+                OnDataChangedEvent();
+            }
+        }
+
+        private int _B;
+        public int B
+        {
+            get { return _B; }
+            set
+            {
+                _B = value;
+                OnDataChangedEvent();
+            }
+        }
+
+        private void OnDataChangedEvent()
+        {
+            if (DataChanged != null)
+                DataChanged();
+        }
+
+
+        //Events 
+        public event DataChangedHandler DataChanged;
+
+    }
+
+}
---
+++ EventsNDeligateBasics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventsNDeligateBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new DataSet();
+            data.A = 3;
+            data.B = 4;
+
+            var calculator = new Calculator(data);
+
+            data.A = 6; // This will 
+
+            // Unsubscribe from all events.
+            calculator.Dispose();
+        }
+    }
+}
